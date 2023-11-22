27988849ed8cde3107c13b1d6866269fe5f964ec
khyungjun <kpos7503@naver.com>
2023-11-23 02:40:32 +0900

    Init

---
+++ src/main/java/com/batch/test/springboot/BatchTestApplication.java
@@ -0,0 +1,13 @@
+package com.batch.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/test/springboot/BatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.test.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
